9338fc0fa3a5b39cb25a5e2c372f8373e5a365f2
windypermadi <whindy21official@gmail.com>
2023-01-12 13:44:29 +0700

    asd

--- api/st_list_transaction_profile.php
+++ api/st_list_transaction_profile.php
@@ -26,9 +26,9 @@ if (isset($id_login)) {
             foreach ($data as $key) {
 
                 $getjumlah_produk = $conn->query("SELECT count(id_transaksi) as jumlah_produk FROM transaksi_detail
-                WHERE id_transaksi = '$key[id_transaksi]'")->fetch_object;
-                var_dump($getjumlah_produk);
-                die;
+                WHERE id_transaksi = '27971841477837838'")->fetch_object;
+                // var_dump($getjumlah_produk);
+                // die;
                 if ($getjumlah_produk->jumlah_produk > 1) {
                     $status_lebih_satu = 'Y';
                     $keterangan_lebih_satu = $getjumlah_produk->jumlah_produk . ' produk lainnya';
