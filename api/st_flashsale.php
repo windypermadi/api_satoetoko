0d11f826d62940bb1feb46b5ba292e6e88232445
windypermadi <whindy21official@gmail.com>
2023-01-16 15:03:34 +0700

    update

--- api/st_flashsale.php
+++ api/st_flashsale.php
@@ -6,54 +6,59 @@ $response = new Response();
 $limit = $_GET['limit'];
 $offset = $_GET['offset'];
 
-$result2 = array();
-$data = $conn->query("SELECT a.id_master, a.image_master, a.judul_master, a.harga_master, a.diskon_rupiah, a.diskon_persen,
-    a.total_dibeli, a.total_disukai, SUM(b.jumlah) as jumlah , a.id_sub_kategori, c.nama_kategori, a.status_master_detail, a.status_varian
-    FROM master_item a JOIN stok b ON a.id_master = b.id_barang
-    JOIN kategori_sub c ON a.id_sub_kategori = c.id_sub
-    LEFT JOIN master_buku_detail d ON a.id_master = d.id_master
-	LEFT JOIN master_fisik_detail e ON a.id_master = e.id_master
-    WHERE a.status_aktif = 'Y' AND a.status_approve = '2' AND a.status_hapus = 'N' AND (d.id_master IS NOT NULL OR e.id_master IS NOT NULL) GROUP BY a.id_master ORDER BY a.tanggal_approve DESC LIMIT $offset, $limit");
+$data = $conn->query("SELECT * FROM flashsale a 
+JOIN flashsale_detail b ON a.id_flashsale = b.kd_flashsale
+JOIN master_item c ON b.kd_barang = c.id_master
+WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N'");
 foreach ($data as $key => $value) {
 
     //! untuk varian harga diskon atau enggak
-    if ($value['status_varian'] == 'Y') {
-        $status_varian_diskon = 'UPTO'; 
-        $varian = $conn->query("SELECT *, (harga_varian-diskon_rupiah_varian) as harga_varian_final FROM variant WHERE id_master = '$value[id_master]' ORDER BY harga_varian_final ASC")->fetch_all(MYSQLI_ASSOC);
-        // foreach ($varian as $key => $value) {
-        // }
-        $min_normal = $varian[0]['harga_varian'];
-        $max_normal = $varian[count($varian) - 1]['harga_varian'];
-
-        $min = $varian[0]['harga_varian_final'];
-        $max = $varian[count($varian) - 1]['harga_varian_final'];
-
-        $jumlah_diskon = $varian[count($varian) - 1]['diskon_persen_varian'];
-
-        //! varian ada diskon
-        if ($varian[0]['diskon_rupiah_varian'] != 0) {
-            $status_diskon = 'Y';
-            (float)$harga_disc = $varian->harga_varian - $varian->diskon_rupiah_varian;
-        } else {
-            $status_diskon = 'N';
-            (float)$harga_disc = $varian->diskon_rupiah_varian;
-        }
-
-        $harga_produk = rupiah($min_normal) . " - " . rupiah($max_normal);
-        $harga_tampil = rupiah($min) . " - " . rupiah($max);
-    } else {
-        $jumlah_diskon = $value['diskon_persen'];
-        $status_varian_diskon = 'OFF';
-        if ($value['diskon_persen'] != 0) {
-            $status_diskon = 'Y';
-            (float)$harga_disc = $value['harga_master'] - $value['diskon_rupiah'];
-        } else {
-            $status_diskon = 'N';
-            (float)$harga_disc = $value['harga_master'];
-        }
-
+    if ($value['diskon'] != 0) {
+        $status_diskon = 'Y';
         $harga_produk = rupiah($value['harga_master']);
-        $harga_tampil = rupiah($harga_disc);
+        $harga_tampil = rupiah($value['harga_master'] - $value['diskon']);
+
+        // $status_varian_diskon = 'UPTO'; 
+        // $varian = $conn->query("SELECT *, (harga_varian-diskon_rupiah_varian) as harga_varian_final FROM variant WHERE id_master = '$value[id_master]' ORDER BY harga_varian_final ASC")->fetch_all(MYSQLI_ASSOC);
+        // // foreach ($varian as $key => $value) {
+        // // }
+        // $min_normal = $varian[0]['harga_varian'];
+        // $max_normal = $varian[count($varian) - 1]['harga_varian'];
+
+        // $min = $varian[0]['harga_varian_final'];
+        // $max = $varian[count($varian) - 1]['harga_varian_final'];
+
+        // $jumlah_diskon = $varian[count($varian) - 1]['diskon_persen_varian'];
+
+        // //! varian ada diskon
+        // if ($varian[0]['diskon_rupiah_varian'] != 0) {
+        //     $status_diskon = 'Y';
+        //     (float)$harga_disc = $varian->harga_varian - $varian->diskon_rupiah_varian;
+        // } else {
+        //     $status_diskon = 'N';
+        //     (float)$harga_disc = $varian->diskon_rupiah_varian;
+        // }
+
+        // $harga_produk = rupiah($min_normal) . " - " . rupiah($max_normal);
+        // $harga_tampil = rupiah($min) . " - " . rupiah($max);
+    } else {
+
+        $status_diskon = 'N';
+        $harga_produk = rupiah($value['harga_master']);
+        $harga_tampil = rupiah($value['harga_master']);
+        
+        // $jumlah_diskon = $value['diskon_persen'];
+        // $status_varian_diskon = 'OFF';
+        // if ($value['diskon_persen'] != 0) {
+        //     $status_diskon = 'Y';
+        //     (float)$harga_disc = $value['harga_master'] - $value['diskon_rupiah'];
+        // } else {
+        //     $status_diskon = 'N';
+        //     (float)$harga_disc = $value['harga_master'];
+        // }
+
+        // $harga_produk = rupiah($value['harga_master']);
+        // $harga_tampil = rupiah($harga_disc);
     }
 
     $status_jenis_harga = '1';
@@ -65,21 +70,21 @@ foreach ($data as $key => $value) {
     }
 
     $data_produk[] = [
-    'id_master' => '',
-    'image_master' => '',
-    'judul_master' => '',
-    'harga_produk' => '',
-    'harga_tampi;' => '',
-    'status_diskon' => '',
-    'diskon' => '',
-    'stok_total' => '',
-    'sisa_stok' => ''
+    'id_master' => $value['id_master'],
+    'image_master' => $value['image_master'],
+    'judul_master' => $value['judul_master'],
+    'harga_produk' => $harga_produk,
+    'harga_tampil;' => $harga_tampil,
+    'status_diskon' => $status_diskon,
+    'diskon' => $value['diskon'],
+    'stok_total' => $value['stok_flashdisk'],
+    'sisa_stok' => $value['stok_terjual_flashdisk']
 ];
 
     $result2[] = [
-        'waktu_mulai' => $value['id_master'],
-        'waktu_selesai' => $value['judul_master'],
-        'status_tampil' => $imagegambar,
+        'waktu_mulai' => $value['waktu_mulai'],
+        'waktu_selesai' => $value['waktu_selesai'],
+        'status_tampil' => 'Y',
         'data_produk' => $data_produk
     ];
 }
