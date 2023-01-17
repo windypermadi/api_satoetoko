1fc221c49e32d8d127814e094b80cf309b7c7004
windypermadi <whindy21official@gmail.com>
2023-01-17 14:35:27 +0700

    dasda

--- api/st_masterdetail.php
+++ api/st_masterdetail.php
@@ -11,17 +11,18 @@ if (isset($id_master)) {
     $cekitemdata = $conn->query($datamaster);
     $data = $cekitemdata->fetch_object();
 
-    $sekarang = $conn->query("SELECT * FROM flashsale a 
+    $sekarang = "SELECT * FROM flashsale a 
     JOIN flashsale_detail b ON a.id_flashsale = b.kd_flashsale
     JOIN master_item c ON b.kd_barang = c.id_master
-    WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND b.kd_barang = '$id_master' AND a.waktu_mulai < NOW() AND a.waktu_selesai > NOW()")->fetch_object;
+    WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND b.kd_barang = '$id_master' AND a.waktu_mulai < NOW() AND a.waktu_selesai > NOW()";
+    $ceksekarang = $conn->query($flashsale_detail)->nums_row();
 
     $akandatang = $conn->query("SELECT * FROM flashsale a 
         JOIN flashsale_detail b ON a.id_flashsale = b.kd_flashsale
         JOIN master_item c ON b.kd_barang = c.id_master
         WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND b.kd_barang = '$id_master' AND a.waktu_mulai > NOW() AND a.waktu_selesai > NOW()")->fetch_object;
 
-    if (!empty($sekarang->id_flashsale)) {
+    if ($ceksekarang > 0) {
         $status_flashsale = '2';
     } else {
         if (!empty($akandatang->id_flashsale)) {
