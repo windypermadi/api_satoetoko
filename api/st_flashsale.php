8f3d061ef9c88a61f353874ca1b7ee6a3f43e72b
windypermadi <whindy21official@gmail.com>
2023-01-17 16:12:45 +0700

    adsad

--- api/st_flashsale.php
+++ api/st_flashsale.php
@@ -10,31 +10,36 @@ switch ($tag) {
         $data = $conn->query("SELECT * FROM flashsale WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND waktu_selesai > NOW()");
         foreach ($data as $key => $value) {
 
-            //! untuk varian harga diskon atau enggak
-            if ($value['diskon'] != 0) {
-                $status_diskon = 'Y';
-                $harga_produk = rupiah($value['harga_master']);
-                $harga_tampil = rupiah($value['harga_master'] - $value['diskon']);
-            } else {
-
-                $status_diskon = 'N';
-                $harga_produk = rupiah($value['harga_master']);
-                $harga_tampil = rupiah($value['harga_master']);
-            }
-
-            $status_jenis_harga = '1';
-
-            if ($value['status_master_detail'] == '2') {
-                $imagegambar = $getimagebukufisik . $value['image_master'];
-            } else {
-                $imagegambar = $getimagefisik . $value['image_master'];
-            }
-
             $dataproduct = $conn->query("SELECT * FROM flashsale a 
-    JOIN flashsale_detail b ON a.id_flashsale = b.kd_flashsale
-    JOIN master_item c ON b.kd_barang = c.id_master
-    WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' LIMIT 3");
+            JOIN flashsale_detail b ON a.id_flashsale = b.kd_flashsale
+            JOIN master_item c ON b.kd_barang = c.id_master
+            WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' LIMIT 3");
             foreach ($dataproduct as $key => $key2) {
+
+                // $cekflash = $conn->query("SELECT * FROM flashsale a 
+                //     JOIN flashsale_detail b ON a.id_flashsale = b.kd_flashsale
+                //     JOIN master_item c ON b.kd_barang = c.id_master
+                //     WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND b.kd_barang = '$key2[id_master]' AND a.waktu_mulai < NOW() AND a.waktu_selesai > NOW()")->fetch_object();
+
+                //! untuk varian harga diskon atau enggak
+                if ($key2['diskon'] != 0) {
+                    $status_diskon = 'Y';
+                    $harga_produk = rupiah($key2['harga_master']);
+                    $harga_tampil = rupiah($key2['harga_master'] - ($key2['harga_master'] * ($key2['diskon'] / 10)));
+                } else {
+                    $status_diskon = 'N';
+                    $harga_produk = rupiah($key2['harga_master']);
+                    $harga_tampil = rupiah($key2['harga_master'] - ($key2['harga_master'] * ($key2['diskon'] / 10)));
+                }
+
+                $status_jenis_harga = '1';
+
+                if ($key2['status_master_detail'] == '2') {
+                    $imagegambar = $getimagebukufisik . $key2['image_master'];
+                } else {
+                    $imagegambar = $getimagefisik . $key2['image_master'];
+                }
+
                 $data_produk[] = [
                     'id_master' => $key2['id_master'],
                     'image_master' => $key2['image_master'],
