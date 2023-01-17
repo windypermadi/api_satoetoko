72b5cba53148ad1edb3709b4dcff26061d05cc3c
windypermadi <whindy21official@gmail.com>
2023-01-17 14:42:26 +0700

    adsd

--- api/st_masterdetail.php
+++ api/st_masterdetail.php
@@ -17,15 +17,16 @@ if (isset($id_master)) {
     WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND b.kd_barang = '$id_master' AND a.waktu_mulai < NOW() AND a.waktu_selesai > NOW()";
     $ceksekarang = $conn->query($sekarang)->nums_row;
 
-    $akandatang = $conn->query("SELECT * FROM flashsale a 
+    $akandatang = "SELECT * FROM flashsale a 
         JOIN flashsale_detail b ON a.id_flashsale = b.kd_flashsale
         JOIN master_item c ON b.kd_barang = c.id_master
-        WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND b.kd_barang = '$id_master' AND a.waktu_mulai > NOW() AND a.waktu_selesai > NOW()")->fetch_object;
+        WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND b.kd_barang = '$id_master' AND a.waktu_mulai > NOW() AND a.waktu_selesai > NOW()";
+    $cekakandatang = $conn->query($akandatang)->nums_row;
 
     if ($ceksekarang > 0) {
         $status_flashsale = '2';
     } else {
-        if (!empty($akandatang->id_flashsale)) {
+        if ($cekakandatang > 0) {
             $status_flashsale = '1';
         } else {
             $status_flashsale = '0';
