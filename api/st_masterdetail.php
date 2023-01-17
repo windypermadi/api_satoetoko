1f849ca2300c7b66f4368518db66c0bec433db46
windypermadi <whindy21official@gmail.com>
2023-01-17 14:39:30 +0700

    sadas

--- api/st_masterdetail.php
+++ api/st_masterdetail.php
@@ -15,7 +15,7 @@ if (isset($id_master)) {
     JOIN flashsale_detail b ON a.id_flashsale = b.kd_flashsale
     JOIN master_item c ON b.kd_barang = c.id_master
     WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND b.kd_barang = '$id_master' AND a.waktu_mulai < NOW() AND a.waktu_selesai > NOW()";
-    $ceksekarang = $conn->query($sekarang)->nums_row();
+    $ceksekarang = $conn->query($sekarang)->nums_row;
 
     $akandatang = $conn->query("SELECT * FROM flashsale a 
         JOIN flashsale_detail b ON a.id_flashsale = b.kd_flashsale
