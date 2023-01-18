f240d7c60ef8aab84e657adae63b37761f360ab5
windypermadi <whindy21official@gmail.com>
2023-01-18 12:15:51 +0700

    bugfix

--- api/st_flashsale.php
+++ api/st_flashsale.php
@@ -118,6 +118,7 @@ switch ($tag) {
     case 'semua':
         $data = $conn->query("SELECT * FROM flashsale WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND waktu_selesai > NOW()");
         foreach ($data as $key => $value) {
+            $i = 0;
 
             $waktusekarang = date("Y-m-d H:i:s");
             $waktu_mulai = $value['waktu_mulai'];
@@ -128,16 +129,15 @@ switch ($tag) {
             // die;
 
             if (($waktu_mulai <= $waktusekarang) && ($waktu_selesai >= $waktusekarang)) {
-                $status_tampil = 'Y';
+                $status_tampil[$i] = 'Y';
             } else {
-                $status_tampil = 'N';
+                $status_tampil[$i] = 'N';
             }
 
             $dataproduct = $conn->query("SELECT *, (stok_flashdisk-stok_terjual_flashdisk) as sisa_stok FROM flashsale a 
             JOIN flashsale_detail b ON a.id_flashsale = b.kd_flashsale
             JOIN master_item c ON b.kd_barang = c.id_master
             WHERE a.id_flashsale = '$value[id_flashsale]'");
-            $i = 0;
             foreach ($dataproduct as $key => $key2) {
 
                 if ($key2['status_varian'] == 'Y') {
@@ -223,16 +223,14 @@ switch ($tag) {
                 ];
                 $i++;
             }
-
-
-            for ($x = 0; $x < count($data_produk); $x++) {
-                $result2[] = [
-                    'waktu_mulai' => $value['waktu_mulai'],
-                    'waktu_selesai' => $value['waktu_selesai'],
-                    'status_tampil' => $status_tampil,
-                    'data_produk' => $data_produk[$x]
-                ];
-            }
+        }
+        for ($x = 0; $x < count($data_produk); $x++) {
+            $result2[] = [
+                'waktu_mulai' => $value['waktu_mulai'],
+                'waktu_selesai' => $value['waktu_selesai'],
+                'status_tampil' => $status_tampil[$x],
+                'data_produk' => $data_produk[$x]
+            ];
         }
 
         if (isset($result2[0])) {
