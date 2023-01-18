1aaae37c4d4cb35d4d7e0aa931d0618b0add9fe2
windypermadi <whindy21official@gmail.com>
2023-01-18 09:20:47 +0700

    asda

--- api/st_flashsale.php
+++ api/st_flashsale.php
@@ -13,7 +13,7 @@ switch ($tag) {
             $dataproduct = $conn->query("SELECT *, (stok_flashdisk-stok_terjual_flashdisk) as sisa_stok FROM flashsale a 
             JOIN flashsale_detail b ON a.id_flashsale = b.kd_flashsale
             JOIN master_item c ON b.kd_barang = c.id_master
-            WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' LIMIT 3");
+            WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND a.waktu_mulai < NOW() AND a.waktu_selesai > NOW() LIMIT 3");
             foreach ($dataproduct as $key => $key2) {
 
                 // $cekflash = $conn->query("SELECT * FROM flashsale a 
