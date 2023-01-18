25b572444ce6a5edbdbb2afb9bcafa1f7b7bfd65
windypermadi <whindy21official@gmail.com>
2023-01-18 12:12:05 +0700

    update

--- api/st_flashsale.php
+++ api/st_flashsale.php
@@ -127,7 +127,7 @@ switch ($tag) {
             // var_dump($waktu_mulai);
             // die;
 
-            if (($waktu_mulai < $waktusekarang)) {
+            if (($waktu_mulai <= $waktusekarang) && ($waktu_selesai >= $waktusekarang)) {
                 $status_tampil = 'Y';
             } else {
                 $status_tampil = 'N';
@@ -137,6 +137,7 @@ switch ($tag) {
             JOIN flashsale_detail b ON a.id_flashsale = b.kd_flashsale
             JOIN master_item c ON b.kd_barang = c.id_master
             WHERE a.id_flashsale = '$value[id_flashsale]'");
+            $i = 0;
             foreach ($dataproduct as $key => $key2) {
 
                 if ($key2['status_varian'] == 'Y') {
@@ -209,7 +210,7 @@ switch ($tag) {
                 //     JOIN master_item c ON b.kd_barang = c.id_master
                 //     WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND b.kd_barang = '$key2[id_master]' AND a.waktu_mulai < NOW() AND a.waktu_selesai > NOW()")->fetch_object();
 
-                $data_produk[] = [
+                $data_produk[$i][] = [
                     'id_master' => $key2['id_master'],
                     'image_master' => $imagegambar,
                     'judul_master' => $key2['judul_master'],
@@ -220,14 +221,18 @@ switch ($tag) {
                     'stok_total' => $key2['stok_flashdisk'],
                     'sisa_stok' => $key2['stok_terjual_flashdisk']
                 ];
+                $i++;
             }
 
-            $result2[] = [
-                'waktu_mulai' => $value['waktu_mulai'],
-                'waktu_selesai' => $value['waktu_selesai'],
-                'status_tampil' => $status_tampil,
-                'data_produk' => $data_produk
-            ];
+
+            for ($x = 0; $x < count($data_produk); $x++) {
+                $result2[] = [
+                    'waktu_mulai' => $value['waktu_mulai'],
+                    'waktu_selesai' => $value['waktu_selesai'],
+                    'status_tampil' => $status_tampil,
+                    'data_produk' => $data_produ[$x]
+                ];
+            }
         }
 
         if (isset($result2[0])) {
