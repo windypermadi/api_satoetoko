c9afab81296e6a1c2eab697f1cb6e9b0d467b4d7
windypermadi <whindy21official@gmail.com>
2023-01-19 10:08:51 +0700

    update

--- api/st_flashsale.php
+++ api/st_flashsale.php
@@ -7,13 +7,17 @@ $tag = $_GET['tag'];
 
 switch ($tag) {
     case 'home':
-        $data = $conn->query("SELECT * FROM flashsale WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND waktu_selesai > NOW()");
+        $data = $conn->query("SELECT * FROM flashsale WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND waktu_mulai >= CURRENT_DATE AND waktu_mulai <= CURRENT_TIME AND waktu_selesai >= CURRENT_DATE AND waktu_selesai >= CURRENT_TIME");
         foreach ($data as $key => $value) {
 
+            // $dataproduct = $conn->query("SELECT *, (stok_flashdisk-stok_terjual_flashdisk) as sisa_stok FROM flashsale a 
+            // JOIN flashsale_detail b ON a.id_flashsale = b.kd_flashsale
+            // JOIN master_item c ON b.kd_barang = c.id_master
+            // WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND a.waktu_mulai < NOW() AND a.waktu_selesai > NOW() LIMIT 3");
             $dataproduct = $conn->query("SELECT *, (stok_flashdisk-stok_terjual_flashdisk) as sisa_stok FROM flashsale a 
             JOIN flashsale_detail b ON a.id_flashsale = b.kd_flashsale
             JOIN master_item c ON b.kd_barang = c.id_master
-            WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND a.waktu_mulai < NOW() AND a.waktu_selesai > NOW() LIMIT 3");
+            WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND a.waktu_mulai >= CURRENT_DATE AND a.waktu_mulai <= CURRENT_TIME AND a.waktu_selesai >= CURRENT_DATE AND a.waktu_selesai >= CURRENT_TIME LIMIT 3");
             foreach ($dataproduct as $key => $key2) {
 
                 if ($key2['status_varian'] == 'Y') {
@@ -116,7 +120,7 @@ switch ($tag) {
         }
         break;
     case 'semua':
-        $data = $conn->query("SELECT * FROM flashsale WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND waktu_selesai > NOW()");
+        $data = $conn->query("SELECT * FROM flashsale WHERE status_tampil_waktu = 'Y' AND status_remove_flashsale = 'N' AND waktu_selesai >= CURRENT_DATE AND waktu_selesai >= CURRENT_TIME");
         $i = 0;
 
         foreach ($data as $key => $value) {
