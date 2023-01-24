541e5f4a13f84aee03be12064fcb54dc0e709dd9
windypermadi <whindy21official@gmail.com>
2023-01-24 15:57:01 +0700

    upadte

--- api/cekOngkir.php
+++ api/cekOngkir.php
@@ -22,9 +22,8 @@ curl_setopt_array($curl, array(
     CURLOPT_HTTPHEADER => array(
         'secret-access-key: SYRzSiaro+a9U0wa5munOw==',
         'access-key-id: Anteraja_x_satoe_SIT',
-        'Content-Type: application/json',
-        'Cookie: AWSALB=QKJDW5IXgvhYjhdojtKEi8uXFaIqHije5rWaQ+gQlz34O4Ybp+8E+DhB2nBhicGRZ5m7BhCXFAYpUZvRobITwyQTBvcd/2ONt99bHyelkhlCevGDOFZl8jjFkkiZ; AWSALBCORS=QKJDW5IXgvhYjhdojtKEi8uXFaIqHije5rWaQ+gQlz34O4Ybp+8E+DhB2nBhicGRZ5m7BhCXFAYpUZvRobITwyQTBvcd/2ONt99bHyelkhlCevGDOFZl8jjFkkiZ; AWSALBTG=htYXCGidci5dV2PKpA1n8pHcz9lEbNi53HFQNpeFvfKXt5f2b2i0MS0IKNlT/cL1HTGsuW+lfgNmr3F6u8cB9T9Um43gSBOHZ+hU3DaHSu8XscYos4vykIqy9ANjvXsVoHHOkvfIK2gRSIk4lNXwaGE4xTTtYa9mZZ7bXRrVk3w8LABeamo=; AWSALBTGCORS=htYXCGidci5dV2PKpA1n8pHcz9lEbNi53HFQNpeFvfKXt5f2b2i0MS0IKNlT/cL1HTGsuW+lfgNmr3F6u8cB9T9Um43gSBOHZ+hU3DaHSu8XscYos4vykIqy9ANjvXsVoHHOkvfIK2gRSIk4lNXwaGE4xTTtYa9mZZ7bXRrVk3w8LABeamo='
-    ),
+        'Content-Type: application/json'
+    )
 ));
 
 $response = curl_exec($curl);
